c3f5c2cb0ecd92728221abf2f868b8618cf9cf01
Josh Manders <josh@joshmanders.com>
2024-06-29 19:42:43 -0500

    remove old welcome page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('welcome');
-Route::view('/home', 'pages.home')->name('home');
+Route::view('/', 'pages.home')->name('home');
 Route::view('/profile', 'pages.profile')->name('profile');
 Route::view('/post', 'pages.post')->name('post');
